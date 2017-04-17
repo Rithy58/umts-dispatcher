02aae8691c22f1110920242c11d7e04991eb8260
Matt Robinson <mattrobinson125@gmail.com>
2017-04-17 12:14:41 -0400

    Remove old code I missed in staging.

--- modules/db.js
+++ modules/db.js
@@ -1,9 +1,5 @@
 var pg = require('pg');
 
-var timestampOID = 1114;
-pg.types.setTypeParser(1114, function(stringValue) {
-  return stringValue;
-})
 var config = {
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
