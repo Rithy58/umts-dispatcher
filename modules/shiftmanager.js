c0922bb99e80d08b096cbff6627c3ab960ce20c9
Matt Robinson <mattrobinson125@gmail.com>
2017-04-17 12:15:12 -0400

    Refactored shifts to return a promise for processing in the socket.

--- modules/shiftmanager.js
+++ modules/shiftmanager.js
@@ -2,7 +2,8 @@ var db = require('./db');
 
 module.exports = {
 
-  // Returns an array of shifts that occur on a given day.
+  // Returns an promise for querying the database for
+  // shifts occuring on a given day.
   // @param {Date} date
   getShiftByDay: function(date) {
     if(!date) {  // If no date is passed, return todays info
@@ -10,13 +11,6 @@ module.exports = {
     }
     query = "SELECT * FROM shift WHERE date_trunc('day', start_time) = make_timestamptz($1,$2,$3,0,0,0)";
     args = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
-    return queryDb(query, args);
-    db.query(query, args)
-    .then((res) => {
-      return res.rows;
-    })
-    .catch((err) => {
-      console.error('error running query', err);
-    });
+    return db.query(query, args);
   },
 };
