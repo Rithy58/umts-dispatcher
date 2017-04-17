fcea2bfad6c5103363f6e60fc802d57eaf5eb9e6
Matt Robinson <mattrobinson125@gmail.com>
2017-04-17 11:47:11 -0400

    Added getShiftByDay to shift manager

--- modules/shiftmanager.js
+++ modules/shiftmanager.js
@@ -1,5 +1,22 @@
+var db = require('./db');
+
 module.exports = {
-  test: function() {
-    console.log("Hello from shift manager module");
-  }
+
+  // Returns an array of shifts that occur on a given day.
+  // @param {Date} date
+  getShiftByDay: function(date) {
+    if(!date) {  // If no date is passed, return todays info
+      date = new Date();
+    }
+    query = "SELECT * FROM shift WHERE date_trunc('day', start_time) = make_timestamptz($1,$2,$3,0,0,0)";
+    args = [date.getFullYear(), date.getMonth() + 1, date.getDate()];
+    return queryDb(query, args);
+    db.query(query, args)
+    .then((res) => {
+      return res.rows;
+    })
+    .catch((err) => {
+      console.error('error running query', err);
+    });
+  },
 };
