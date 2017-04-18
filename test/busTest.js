7e817896447edd77099570519c4c61ff5a975d01
Matt Robinson <mattrobinson125@gmail.com>
2017-04-18 12:53:15 -0400

    Refactored 'before' testing, added getDefects test

--- test/busTest.js
+++ test/busTest.js
@@ -10,18 +10,39 @@ bus.setDB('./testDB');
 
 describe('busManager', function(){
 
-	before(function() {
-		var queryStr = ""
-		queryStr += "DELETE FROM bus;";
-		queryStr += "INSERT INTO bus (id,type,defects) VALUES ('123', 'long','none'), ('456', 'old','none'), ('789', 'new','none');";
-		db.query(queryStr);
+	// Reset bus table after testing each function.
+	afterEach(function() {
+		return db.query("DELETE FROM bus;");
 	});
 
 	describe('#getAllBuses()', function() {
+
+		before(function() {
+			var queryStr = "INSERT INTO bus (id,type,defects) VALUES ";
+			queryStr += "('123', 'long','none'), ('456', 'old','none'), ('789', 'new','none');";
+			return db.query(queryStr);
+		});
+
 		it('should gets all buses', function() {
 			return bus.getAllBuses()
 			.then(function(res) {
 				assert(res.rowCount == 3, 'Result should contain 3 buses.');
+			});
+		});
+	});
+
+	describe('#getDefects()', function() {
+
+		before(function() {
+			var queryStr = "INSERT INTO bus (id,type,defects) VALUES ";
+			queryStr += "('123', 'long','none'), ('456', 'old','none'), ('789', 'new','none');";
+			return db.query(queryStr);
+		});
+
+		it('should retrieve the defect string', function() {
+			return bus.getDefects(123)
+			.then(function(res) {
+				assert(res.rows[0].defects == 'none', "Result should have defect string say 'none'.");
 			});
 		});
 	});
