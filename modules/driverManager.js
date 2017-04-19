cadc0deee54e2e9ab101b2fc053242db3c30116d
normana10 <arthurnormand@optonline.net>
2017-04-18 20:31:13 -0400

    Filled in first implementation of methods in "driverManager.js" file

--- modules/driverManager.js
+++ modules/driverManager.js
@@ -6,14 +6,31 @@ module.exports = {
   },
 
   getallDrivers: function(){
+    QUERY = "SELECT * From driver";
+    return db.query(QUERY);
   },
 
-  addDriver: function(busID, Type){
+  addDriver: function(name, phone, late_count){
+    QUERY = "INSERT INTO driver (name, phone, late_count) VALUES ($1,$2,$3) RETURNING *";
+    ARGS = [name, phone, late_count];
+    return db.query(QUERY, ARGS);
   },
 
-  removeDriver: function(busID){
+  removeDriver: function(driverID){
+    QUERY = "DELETE FROM driver WHERE id=$1 RETURNING true AS success";
+    ARGS = [driverID];
+    return db.query(QUERY, ARGS);
   },
 
-  getDriverByID: function(busID){
+  updateDriver: function(driverID, Defect){
+    QUERY = "UPDATE driver SET name=$2 phone=$3 late_count=$4 WHERE id=$1 RETURNING *";
+    ARGS = [driverID, name, phone, late_count];
+    return db.query(,[driverID,Defect]);
+  },
+
+  getDriverByID: function(driverID){
+    QUERY = "SELECT * FROM driver WHERE id=$1";
+    ARGS = [driverID];
+    return db.query(QUERY, ARGS);
   }
 }
