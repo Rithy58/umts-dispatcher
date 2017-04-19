43e7b173467e33f3a59d1c0ee747e44f43842b91
normana10 <arthurnormand@optonline.net>
2017-04-18 21:35:09 -0400

    Bugfix: Woops.... added QUERY to updateDriver method

--- modules/driverManager.js
+++ modules/driverManager.js
@@ -25,7 +25,7 @@ module.exports = {
   updateDriver: function(driverID, name, phone, late_count){
     QUERY = "UPDATE driver SET name=$2 phone=$3 late_count=$4 WHERE id=$1 RETURNING *";
     ARGS = [driverID, name, phone, late_count];
-    return db.query(,[driverID,Defect]);
+    return db.query(QUERY,[driverID,Defect]);
   },
 
   getDriverByID: function(driverID){
