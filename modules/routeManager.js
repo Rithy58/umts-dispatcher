6bae9d2b164771766cc7a0205028e0d3465e6e22
Ivanz7 <yifanzhu@umass.edu>
2017-04-18 20:57:11 -0400

    add routeManager.js

---
+++ modules/routeManager.js
@@ -0,0 +1,60 @@
+var db = require('./db');
+
+module.exports = {
+  setDB: function(newDb) {
+    db = require(newDb);
+  },
+
+  addRoute: function(){
+
+  },
+
+  getRouteNumber: function(){
+
+  },
+
+  getAvailableDetours: function(){
+
+  },
+
+  getCurrentDetours: function(){
+
+  },
+
+  getValidBusTrpes: function(){
+
+  },
+
+  getAllRoutes: function(){
+
+  },
+
+  editRouteNumber: function(){
+
+  }.
+
+  editValidBusTypes: function(){
+
+  },
+
+  deleteDetour: function(){
+
+  },
+
+  createDetour: function(){
+
+  },
+
+  avtiveDetour: function(){
+
+  },
+
+  editDetourPath: function(){
+
+  }
+
+
+
+
+
+}
