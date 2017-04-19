3a3da3eca3c564784af41c06b513a79739be44d3
Junqing Qiao <junqingqiao@gmail.com>
2017-04-18 22:28:28 -0400

    Added broken tests for setDriver, setBus, and setRoute functions.

--- test/shiftTest.js
+++ test/shiftTest.js
@@ -19,11 +19,8 @@ describe('shiftManager', function(){
     return db.query("DELETE FROM bus;");
   });
 
-
-
-
   describe('#setDriver()', function() {
-		//Insert 3 entries into the database
+		//Insert entry into the database
 		before(function() {
       db.query("ALTER SEQUENCE {tablename}_id_seq RESTART;")
 			db.query("DELETE FROM shift;");
@@ -32,11 +29,47 @@ describe('shiftManager', function(){
 			return db.query(queryStr);
 		});
 
-		it('should set driver for specific shift to 13', function() {
-			return shift.setDriver('123', '13')
+		it('should set driver for shift #1 to 13', function() {
+			return shift.setDriver('1', '13')
 			.then(function(res) {
 				assert.equal('13', res.rows[0].driver_id);
 			});
 		});
 	});
+
+  describe('#setBus()', function() {
+    // insert entry into database
+		before(function() {
+      db.query("ALTER SEQUENCE {tablename}_id_seq RESTART;")
+			db.query("DELETE FROM shift;");
+			var queryStr = "INSERT INTO shift (start_time, end_time, start_location, end_location, driver_id, bus_id, route) VALUES ";
+			queryStr += "('2012-02-18 14:28:32','2012-02-18 14:28:33', 'Campus Center', 'CS Building', '12', '123', '1');";
+			return db.query(queryStr);
+		});
+
+		it('should set bus for shift #1 to 2', function() {
+			return shift.setBus('1', '2')
+			.then(function(res) {
+				assert.equal('2', res.rows[0].bus_id);
+			});
+		});
+	});
+
+  describe('#setRoute()', function() {
+    // insert entry into database
+    before(function() {
+      db.query("ALTER SEQUENCE {tablename}_id_seq RESTART;")
+      db.query("DELETE FROM shift;");
+      var queryStr = "INSERT INTO shift (start_time, end_time, start_location, end_location, driver_id, bus_id, route) VALUES ";
+      queryStr += "('2012-02-18 14:28:32','2012-02-18 14:28:33', 'Campus Center', 'CS Building', '12', '123', '1');";
+      return db.query(queryStr);
+    });
+
+    it('should set route for shift #1 to 95', function() {
+      return shift.setRoute('1', '95')
+      .then(function(res) {
+        assert.equal('95', res.rows[0].route);
+      });
+    });
+  });
 });
