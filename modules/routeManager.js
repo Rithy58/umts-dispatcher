0456c558d1f2aae9508259eac2fdebb3093422de
Ivanz7 <yifanzhu@umass.edu>
2017-04-18 21:34:35 -0400

    fill in method in  routeManager.js without detour part

--- modules/routeManager.js
+++ modules/routeManager.js
@@ -5,51 +5,51 @@ module.exports = {
     db = require(newDb);
   },
 
-  addRoute: function(){
+  addRoute: function(routeID){
+    QUERY = "INSERT INTO route "
+  },
+
+  getRouteNumber: function(routeID){
+    return db.query("SELECT number FROM route WHERE id=$1", [routeID]);
+  },
+
+  getAvailableDetours: function(routeID){
 
   },
 
-  getRouteNumber: function(){
+  getCurrentDetours: function(routeID){
 
   },
 
-  getAvailableDetours: function(){
-
-  },
-
-  getCurrentDetours: function(){
-
-  },
-
-  getValidBusTrpes: function(){
-
+  getValidBusTrpes: function(routeID){
+    return db.query("SELECT valid_bus_types FROM route WHERE id=$1", [routeID]);
   },
 
   getAllRoutes: function(){
-
+    return db.query("SELECT * FROM route");
   },
 
-  editRouteNumber: function(){
-
+  editRouteNumber: function(routeID, new_number){
+    return db.query("UPDATE route SET number = new_number WHERE id=$1", [routeID]);
   }.
 
-  editValidBusTypes: function(){
+  editValidBusTypes: function(routeID, new_bus_types){
+    return db.query("UPDATE route SET valid_bus_types = new_bus_types WHERE id=$1", [routeID]);
+  },
+
+  deleteDetour: function(routeID, detourID){
 
   },
 
-  deleteDetour: function(){
+  createDetour: function(routeID, detour){
 
   },
 
-  createDetour: function(){
+  avtiveDetour: function(routeID, detourID, state){
 
   },
 
-  avtiveDetour: function(){
-
-  },
-
-  editDetourPath: function(){
+  editDetourPath: function(routeID, detourID, path){
 
   }
 
