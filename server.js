eeacf738e48c4f68173fa84e5cd18806d55d28d6
Matt Robinson <mattrobinson125@gmail.com>
2017-04-21 02:14:39 -0400

    Added scaffolding for shift-management, imported angular material, connected to socket. Deleted some stuff the CLI put in.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 // Import data access components
-var shift = require('./modules/shiftmanager');
+var shift = require('./modules/shiftManager');
 var bus = require('./modules/busManager');
 
 
@@ -13,18 +13,26 @@ app.set('port', process.env.PORT || 8080);
 
 app.use(express.static('./dist'));
 
+// For now lets redirect everyone to the homepage
+app.get('*', function (req, res) {
+    res.redirect('/');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
 
-  // User Disconnected
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  // User sends an example message
+  socket.on('getShiftByDay', function(date){
+    console.log(date);
+    shift.getShiftByDay(new Date(date))
+    .then(function(res) {
+      io.emit('update shifts', res.rows);
+    })
+    .catch(function(err) {
+      console.error(err);
+    })
   });
 
-  // User sends an example message
-  socket.on('example message', function(msg){
-    console.log(msg);
-  });
 });
 
 var server = http.listen(app.get('port'), function() {
