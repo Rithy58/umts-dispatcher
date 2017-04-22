f68d570899505f64107f1fe4769d69837fc25e21
normana10 <arthurnormand@optonline.net>
2017-04-21 20:37:00 -0400

    Driver manager HTML and component edits

--- src/app/driver-management/driver-management.component.ts
+++ src/app/driver-management/driver-management.component.ts
@@ -1,51 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Shift } from './shift';
-import { ShiftService } from './shift.service';
+import { Driver } from './driver';
+import { DriverService } from './driver.service';
 
 @Component({
-  selector: 'shift-management',
-  templateUrl: './shift-management.component.html',
-  styleUrls: [ './shift-management.component.css' ]
+  selector: 'driver-management',
+  templateUrl: './driver-management.component.html',
+  styleUrls: [ './driver-management.component.css' ]
 })
-export class ShiftManagementComponent implements OnInit {
+export class DriverManagementComponent implements OnInit {
 
-  // Clients local Shift[] array for displaying
-  shifts: Shift[] = [];
+  // Clients local Driver[] array for displaying
+  drivers: Driver[] = [];
 
-  // Listener for all shifts
-  getShiftsConnection;
+  // Listener for all drivers
+  getDriversConnection;
 
-  constructor(private ShiftService: ShiftService) { }
+  constructor(private DriverService: DriverService) { }
 
   ngOnInit(): void {
-    this.ShiftService.getShiftByDay(new Date(2017,3,17,0,0,0,0));
-    this.getShiftsConnection = this.ShiftService.getShifts()
+    this.DriverService.getAllDrivers;
+    this.getDriversConnection = this.DriverService.getAllDrivers()
       .subscribe(array => {
         for (let i in array) {
-          // create a Shift object in the front end for each shift JSON.
-          var shift = array[i];
-          let inShiftArr = false;
-          // If this shift is already in the array, just update the data.
-          this.shifts.forEach(function(s) {
-            if (s.id === shift.id) {
-              inShiftArr = true;
-              s.updateData(shift.start_time, shift.end_time,
-                shift.start_location, shift.end_location,
-                shift.route, shift.driver_id, shift.bus_id);
+          // create a Driver object in the front end for each driver JSON.
+          var driver = array[i];
+          let inDriverarr = false;
+          // If this driver is already in the array, just update the data.
+          this.drivers.forEach(function(d) {
+            if (d.id === driver.id) {
+              inDriverarr = true;
+              d.updateData(driver.name, driver.phone, driver.late_count);
             }
           });
           // Insert if we didnt find it.
-          if (inShiftArr === false) {
-            this.shifts.push(new Shift(shift.id, shift.start_time,
-              shift.end_time, shift.start_location, shift.end_location,
-              shift.route, shift.driver_id, shift.bus_id));
+          if (inDriverarr === false) {
+            this.drivers.push(new Driver(driver.id, driver.name, driver.phone, driver.late_count));
           }
         }
       })
   }
 
   ngOnDestroy() {
-    this.getShiftsConnection.unsubscribe();
+    this.getDriversConnection.unsubscribe();
   }
 }
