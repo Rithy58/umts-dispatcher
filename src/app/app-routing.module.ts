641b9b0e41df4b8ca254d615558a8ba16cbf06da
normana10 <arthurnormand@optonline.net>
2017-04-23 16:12:31 -0400

    Uncommented all module imports and paths

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 // Import components here.
 import { ShiftManagementComponent }   from './shift-management/shift-management.component';
 import { BusManagementComponent }   from './bus-management/bus-management.component';
-//import { DriverManagementComponent } from './driver-management/driver-management.component';
-//import { RouteManagementComponent } from './route-management/route-management.component';
+import { DriverManagementComponent } from './driver-management/driver-management.component';
+import { RouteManagementComponent } from './route-management/route-management.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/shifts', pathMatch: 'full' },
   { path: 'shifts',  component: ShiftManagementComponent },
-  //{ path: 'drivers',  component: DriverManagementComponent },
-  //{ path: 'routes',  component: RouteManagementComponent },
+  { path: 'drivers',  component: DriverManagementComponent },
+  { path: 'routes',  component: RouteManagementComponent },
   { path: 'bus',  component: BusManagementComponent },
 ];
 
