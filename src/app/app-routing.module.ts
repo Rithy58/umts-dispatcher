a50b96cb0ad8eee2794e3edca176460c7ba17cf3
Sebastian Lacki <sebastianlacki@Sebastians-MacBook-Pro-2.local>
2017-04-23 08:38:36 -0400

    Front end bus management

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 // Import components here.
 import { ShiftManagementComponent }   from './shift-management/shift-management.component';
+import { BusManagementComponent }   from './bus-management/bus-management.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/shifts', pathMatch: 'full' },
   { path: 'shifts',  component: ShiftManagementComponent },
   // { path: 'drivers',  component: DriverManagementComponent },
   // { path: 'routes',  component: RouteManagementComponent },
-  // { path: 'incidents',  component: IncidentManagementComponent },
+  // { path: 'bus',  component: BusManagementComponent },
 ];
 
 @NgModule({
--- src/app/bus-management/bus.service.ts
+++ src/app/bus-management/bus.service.ts
@@ -1,23 +1,19 @@
-import { Shift } from './shift';
+import { Bus } from './bus';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client';
 
 
 @Injectable()
-export class ShiftService {
+export class BusService {
   private url = "http://localhost:8080";
   private socket = io(this.url);
 
-  getShiftByDay(date) {
-    this.socket.emit('getShiftByDay', date);
-  }
-
-  // This will always load the shifts that have been requested.
+  // This will always load the buses that have been requested.
   // For this data to be updated, a message needs to be sent to the socket
-  getShifts() {
+  getAllBuses() {
     let observable = new Observable(observer => {
-      this.socket.on('update shifts', (data) => {
+      this.socket.on('update buses', (data) => {
         observer.next(data);
       });
       return () => {
