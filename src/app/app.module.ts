d2f78a933969246c99ea2350ba55db577c54d100
Matt Robinson <mattrobinson125@gmail.com>
2017-04-24 10:23:58 -0400

    Somehow this broke but its fixed now

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdSidenavModule, MdListModule,
-  MdToolbarModule, MdIconModule, MdInputModule, MdDialogModule } from '@angular/material';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent }         from './app.component';
 import { SidenavComponent }       from './sidenav/sidenav.component';
